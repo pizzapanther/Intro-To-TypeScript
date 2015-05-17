76594f15729ba989132d49adb891634ffd059c39
Paul Bailey <paul.m.bailey@gmail.com>
2015-05-17 03:08:35 +0000

    finalize stuff

--- ng/app.ts
+++ ng/app.ts
@@ -1,4 +1,22 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
 import {Component, View, bootstrap} from 'angular2/angular2';
 
+// Annotation section
+@Component({
+  selector: 'my-app'
+  // Defines the <my-app></my-app> tag
+})
+@View({
+  template: '<h1>Hello {{ name }}</h1>'
+  // Defines the inline template for the component
+})
+// Component controller
+class MyAppComponent {
+  name: string;
+  
+  constructor() {
+    this.name = 'Paul';
+  }
+}
 
+bootstrap(MyAppComponent);
